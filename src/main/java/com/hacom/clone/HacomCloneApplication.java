876d87407cc51ee340a656afe42c916567d0a133
vinh25042005 <vinh2504123@gmail.com>
2026-01-16 16:06:09 +0700

    feat(app): cho phép liên kết backend và frontend

--- src/main/java/com/hacom/clone/HacomCloneApplication.java
+++ src/main/java/com/hacom/clone/HacomCloneApplication.java
@@ -2,6 +2,9 @@ package com.hacom.clone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HacomCloneApplication {
@@ -10,4 +13,13 @@ public class HacomCloneApplication {
 		SpringApplication.run(HacomCloneApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
